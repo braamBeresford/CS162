cfcf53a32d9e72e7287f903cc6f3963fab5f4356
piphead <beresfob@flip2.engr.oregonstate.edu>
2018-06-05 16:57:11 -0700

    Everything but sort works

--- HW/Code/Assignment 5/list.c
+++ HW/Code/Assignment 5/list.c
@@ -3,36 +3,32 @@
 #include <stdlib.h>
 
 
-int length(struct node* head){
+int length(struct node* head) { //Works
 	int count = 0;
 	int i;
 	int first_time = 0;
 	struct node* current = head;
 	while (1) {
-		if (current != NULL && first_time == 1) {
+		if (current != NULL) {
 			count++;
 			current = current->next;
 		}
-		else if (first_time == 0)
-			;
-		else {
-			count--;
+
+		if (current == NULL)
 			return count;
-		}
-		first_time = 1;
 
 	}
 };
 
 
-void print(struct node * head, int number) {
+void print(struct node * head, int number) { ///Works
 	int i;
 	struct node* current = head;
 	if (head == NULL) {
 		printf("There is nothing in this linked list\n" );
 		return;
 	}
-	current = current->next; //This is to skip head
+
 	for (i = 0; i < number; i++) {
 		if (current != NULL) {
 			printf("%d ", current->val);
@@ -44,52 +40,44 @@ void print(struct node * head, int number) {
 			return;
 		}
 	}
+	putchar('\n');
 
 }
 
-struct node * push(struct node* head, int value) {
+struct node * push(struct node* head, int value) {//works
 	struct node* new_node = malloc(sizeof(struct node));
-
 	new_node->val = value;
-
-	new_node->next = head`;
-	head->next = new_node;
+	new_node->next = head;
+	head = new_node;
 
 	return head;
 
 }
 
-struct node * append(struct node* head, int value) {
+struct node * append(struct node* head, int value) { //works
+	int i;
 	struct node* new_node = malloc(sizeof(struct node));
 	new_node->val = value;
+	new_node->next = NULL;
+
 	struct node* current = head;
 
-	if (head->next == NULL) {
-		head->next = new_node;
-		new_node->next = NULL;
+	if (head == NULL) {
+		head = new_node;
 	}
 
 	else {
-		while (1) {
-			if (current != NULL) {
-
-				current = current->next;
-			}
-
-
-			else {
-
-				current = new_node;
-				new_node->next = NULL;
-				break;
-			}
+		while (current->next != NULL) {
+			current = current->next;
 		}
+
+		current->next = new_node;
 	}
 
 	return head;
 }
 
-struct node * clear(struct node * head) {
+struct node * clear(struct node * head) {//Works
 	struct node* current = head;
 	struct node* next;
 	while (current != NULL) {
@@ -98,30 +86,38 @@ struct node * clear(struct node * head) {
 		current = next;
 	}
 
-	head->next = NULL;
+	head = NULL;
 	return head;
 }
 
-struct node * remove_node(struct node *head, int index) {
+struct node * remove_node(struct node *head, int index) { //works
 	struct node * current = head;
 	struct node * temp;
-	if (index > length(head) - 1) {
+
+	if (index > length(head)) {
 		printf("\nThe index you have provided is not within the bounds\n");
 		return head;
 	}
 
-
 	int i;
-	for (i = 0; i < index; i++) {
+	if(index == 0){
+		temp = current->next;
+		free(current);
+		head = temp;
+		return head;
+	}
+
+	for (i = 0; i < index-1; i++) { // -1 so we are the node before the one being deleted
 		current = current->next;
 	}
 	temp = current->next->next;
 	free(current->next);
-	current->next = temp;
 
+	current->next = temp;
 
 	return head;
 }
+
 
 struct node *sort_ascending(struct node* head) {
 	int i;
@@ -132,15 +128,15 @@ struct node *sort_ascending(struct node* head) {
 		un_sorted = 1;
 		current = head;
 		for (i = 0; i < length(head); i++) {
-			if(current == NULL)
+			if (current == NULL)
 				continue;
-			else if (current->val > current->next->val) { 
+			else if (current->val > current->next->val) {
 				temp = current->next->next->next;
 				current->next->next->next = current->next;
 				current->next = current->next->next;
 				current->next->next->next = temp;
 				un_sorted = 0;
-			}	
+			}
 			current = current->next;
 		}
 
@@ -153,20 +149,26 @@ struct node *sort_ascending(struct node* head) {
 
 struct node * insert_middle(struct node* head, int index, int value) {
 	struct node* current = head;
-	if (index > length(head)) {
+	if (index > length(head)-1) {
 		printf("\nThe index you have chosen is out of range for this list\n");
 		return head;
 	}
+
 	struct node* new_node = malloc(sizeof(struct node));
 	new_node->val = value;
 	struct node* temp;
 
-
-	int i;
-	for (i = 0; i < index; i++) {
-		current = current->next;
+	if(index ==0){
+		temp = head;
+		head = new_node;
+		head->next = temp;
+		return head;
 	}
 
+	int i;
+	for (i = 0; i < index-1; i++) {
+		current = current->next;
+	}
 	temp = current->next;
 	current->next = new_node;
 	new_node->next = temp;
@@ -183,17 +185,8 @@ int main() {
 	head = push(head, 2);
 	head = push(head, 3);
 	head = push(head, 4);
-	// head = push(head, 2);
-	// head = push(head, 1);
-	// head = push(head, 4);
-	// head = push(head, 3);
-	// head = push(head, 4);
-	// head = push(head, 3);
-	// head = push(head, 4);
-	// head = push(head, 3);
-	// head = push(head, 4);
-	// head = push(head, 3);
-	// head = push(head, 4);
+	head = append(head, 5);
+
 
 
 	// printf("Length %d\n", length(head));
@@ -207,10 +200,19 @@ int main() {
 	// head = insert_middle(head, 3, 8);
 
 	// printf("Length %d\n", length(head));
+
 	print(head, 5);
+
+	head = insert_middle(head, 4, 8);
+
+	print(head, 5);
+	// printf("Length %d\n", length(head));
+	// head = remove_node(head, 2);
+	// print(head, 5);
+	// printf("Length %d\n", length(head));
 	// sort_ascending(head);
 	// putchar('\n');
 	// clear(head);
-	print(head, 5);
+	// print(head, 5);
 	return 0;
 }
